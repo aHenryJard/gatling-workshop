dd7bcd77369fb37d01987af4c3235040d0189d2c
aHenryJard <ajard@cloudbees.com>
2020-09-09 17:12:31 +0200

    with feeder

--- src/test/scala/computerdatabase/BasicSimulation.scala
+++ src/test/scala/computerdatabase/BasicSimulation.scala
@@ -8,18 +8,38 @@ class BasicSimulation extends Simulation {
 
   val httpProtocol = http
     .baseUrl("http://localhost:8200") // Here is the root for all relative URLs
+    //.basicAuth("admin","11f0d873624a87b01f98a7259215aca6d8")
 
-  val scn = scenario("Call /casc-bundle/list twice") // A scenario is a chain of requests and pauses
-    .exec(http("list_1")
-    .get("/casc-bundle/list"))
-    .pause(7)
-    .exec(http("list_2")
-    .get("/casc-bundle/list"))
+  val callListScenario = scenario("/casc-bundle/list")
+    .exec(http("list_error_in_purpose")
+    .get("/casc-bundle/list").check(status.is(200)))
+    .pause(1,3)
+    .exec(http("list_refused")
+      .get("/casc-bundle/list").check(status.is(401)))
+    .pause(1,3)
+    .exec(http("list_with_admin")
+    .get("/casc-bundle/list")
+      .basicAuth("admin","11f0d873624a87b01f98a7259215aca6d8").check(status.is(200)))
 
-  setUp(scn.inject(atOnceUsers(1)).protocols(httpProtocol))
+  val bundleNameFeed = csv("bundle-name.csv").random.circular
 
 
+  val oneRegenerateRequest = exec(http("regenerate-token")
+    .get("/casc-bundle/regenerate-token?bundleId=${name}")
+    .basicAuth("admin","11f0d873624a87b01f98a7259215aca6d8")
+    .check(status.is(200)))
+    .pause(1,2)
 
 
+  val refreshTokenScenario = scenario("/casc-bundle/regenerate-token")
+    .feed(bundleNameFeed)
+    .repeat(3) {
+      oneRegenerateRequest
+    }
+
+  setUp(
+    callListScenario.inject(rampUsers(3) during(1 minute)),
+    refreshTokenScenario.inject(atOnceUsers(2))
+  ).protocols(httpProtocol)
 
 }
